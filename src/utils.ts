f5c3b3a28196c56afc9b3fed33ca2685550acc98
Amine Elmouradi <elmouradi.amine.dev@gmail.com>
2024-02-10 16:09:16 +0100

    chore: add eslint and code comments

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
-export function camelToUnderscore(str: string) {
+/**
+ * Converts a camelCase string to underscore_case.
+ *
+ * @param {string} str - The camelCase string to convert.
+ * @returns {string} The string converted to underscore_case.
+ */
+export const camelToUnderscore = (str: string): string => {
   return str.replace(/[A-Z]/g, (match) => `_${match.toLowerCase()}`);
-}
+};
