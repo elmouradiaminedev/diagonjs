4fa7974d6e7ed10377789dad656c0c5a6e771205
Amine Elmouradi <elmouradi.amine.dev@gmail.com>
2024-02-12 02:43:05 +0100

    build: generate cjs and esm formats

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   name: "diagonjs",
   target: "esnext",
+  format: ["cjs", "esm"],
   entry: ["src"],
   splitting: process.env.NODE_ENV === "production",
   minify: process.env.NODE_ENV === "production",
