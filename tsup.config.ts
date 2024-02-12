0821b43f49f4c215d7e6c2ec7aec67490722e2b3
Amine Elmouradi <elmouradi.amine.dev@gmail.com>
2024-02-12 02:58:23 +0100

    fix: generating cjs and esm not required for it work

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   name: "diagonjs",
   target: "esnext",
-  format: ["cjs", "esm"],
   entry: ["src"],
   splitting: process.env.NODE_ENV === "production",
   minify: process.env.NODE_ENV === "production",
